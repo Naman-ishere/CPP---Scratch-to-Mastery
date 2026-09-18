491b9ff102fdc5c5258e9621a4280de0bf1b3359
Naman-isHere <chavdanaman09@gmail.com>
2026-09-19 03:04:47 +0530

    Removed the .exe and .out files.

---
+++ Functions/func1.cpp
@@ -0,0 +1,21 @@
+// How to write the functions?
+
+// TypeOfTheReturningValue <nameOfFunction> {
+
+
+// }
+
+// For example
+
+#include <iostream>
+using namespace std;
+
+void printHello() {
+    cout << "Hello, World!" << endl;
+}
+
+int main() {
+    cout << "Hello, world" << endl;
+    printHello();
+
+}
